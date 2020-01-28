2d4c20149491b3da90a8ef3ee97575eeaf403d4d
CESARDELATORRE <cesardl@microsoft.com>
2020-01-28 01:23:34 +0000

    Spark running on LOCAL and REMOTE AML compute

--- spark-job-simple.py
+++ spark-job-simple.py
@@ -1,9 +1,24 @@
 import pyspark
 import sys
 
+from azureml.core.run import Run
+
+# initialize logger
+run = Run.get_context()
+
 # Below code is Spark 2+
 
 spark = pyspark.sql.SparkSession.builder.appName('test').getOrCreate()
+
+# print runtime versions
+print('****************')
+print('Python version: {}'.format(sys.version))
 print('Spark version: {}'.format(spark.version))
+print('****************')
+
+# log versions
+run.log('PythonVersion', sys.version)
+run.log('SparkVersion', spark.version)
+
 rdd = spark.range(10)
 print(rdd.collect())
